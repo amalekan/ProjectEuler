0522afb17143df6ac7e2dc9a96802b475baf9d79
Alexander Malekan <alexmalekan@gmail.com>
2018-11-12 15:07:47 -0500

    Solution to Problem 46

---
+++ Problem46.py
@@ -0,0 +1,23 @@
+from math import sqrt
+primes = [2]
+def isPrime(n):
+    for x in primes:
+        if x > (sqrt(n)):
+            return True
+        if n%x == 0:
+            return False
+
+for k in range (3,10000):
+    if (isPrime(k)):
+        primes.append(k)
+
+numbers_that_sum = set([x + y for x in primes for y in twice_squares])
+
+x = sorted(numbers_that_sum)
+
+for y in range(9,10000,2):
+    if y in primes:
+        continue
+    if y not in x:
+        print(y)
+        break
